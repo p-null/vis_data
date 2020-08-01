0dfda5a7496a082ad9349117f6e5b36164c1d062
Tingkai Zhang <tingkai.zhang@outlook.com>
2020-08-01 06:00:15 -0400

    update

--- process_emissions.py
+++ process_emissions.py
@@ -10,8 +10,8 @@ for json_item in loaded_json:
     year = int(json_item['year'])
     if year > 1945:
         new_item = json_item
-        new_item['year'] = year + 60
+        new_item['year'] = str(year + 60)
         new_items.append(new_item)
 
 with open('new_emissions.json', '+w') as fo:
-    json.dump(new_items, fo)
+    json.dump(new_items, fo, indent=2)
