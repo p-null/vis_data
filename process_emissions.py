c85977827646c969321f7920cbdbc25820dc4e72
Tingkai Zhang <tingkai.zhang@outlook.com>
2020-08-01 05:56:34 -0400

    update

---
+++ process_emissions.py
@@ -0,0 +1,17 @@
+import json
+
+
+
+with open('emissions.json') as f:
+    loaded_json = json.load(f)
+
+new_items = []
+for json_item in loaded_json:
+    year = int(json_item['year'])
+    if year > 1945:
+        new_item = json_item
+        new_item['year'] = year + 60
+        new_items.append(new_item)
+
+with open('new_emissions.json', '+w') as fo:
+    json.dump(new_items, fo)
